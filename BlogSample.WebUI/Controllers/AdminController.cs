ab3188b14997d1d865bbf0e3eb721884ef256561
korayozdemir <koray_ozdemir@msn.com>
2020-07-21 12:49:28 +0300

    Injection

---
+++ BlogSample.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogSample.BLL.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogSample.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ICategoryService categoryService;
+        public AdminController(ICategoryService _categoryService)
+        {
+            categoryService = _categoryService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryList()
+        {
+            return View(categoryService.getAll());
+        }
+    }
+}
--- BlogSample.WebUI/Startup.cs
+++ BlogSample.WebUI/Startup.cs
@@ -1,9 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using BlogSample.BLL.Abstract;
+using BlogSample.BLL.BlogService;
+using BlogSample.Core.Data.UnitOfWork;
+using BlogSample.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +24,23 @@ namespace BlogSample.WebUI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            var optionsBuilder = new DbContextOptionsBuilder<BlogDbContext>();
+            optionsBuilder.UseSqlServer(Configuration.GetConnectionString("BlogDbConnection"));
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            optionsBuilder.EnableSensitiveDataLogging();
+
+
+            services.AddSingleton<DbContext>(new BlogDbContext(optionsBuilder.Options));
+
+            using (var context =new BlogDbContext(optionsBuilder.Options))
+            {
+                context.Database.EnsureCreated();
+                context.Database.Migrate();
+            }
+
+            services.AddSingleton<IUnitofWork, UnitofWork>();
+            services.AddSingleton<ICategoryService, CategoryService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +62,11 @@ namespace BlogSample.WebUI
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                         name: "CategoryList",
+                          pattern: "Admin/Categories",
+                           defaults: new { controller = "Admin", action = "CategoryList" });
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
